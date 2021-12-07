ead22d6bbce20e1ee0cc95549d3e5f91bfd7ba79
kyuung <mel1521@naver.com>
2021-12-07 20:52:00 +0900

    🚨 fail

--- 1.Queue-Stack/기능개발.js
+++ 1.Queue-Stack/기능개발.js
@@ -1,10 +1,6 @@
 function solution(progresses, speeds) {
-    var answer = [];
-    var bck = 0;
-    var num = 1;
-
-    var q = [];
-    var fnt = 0;
+    let answer = [];
+    let complete = [];
 
     for (var i = 0; i < progresses.length; i++) {
         var day = 0;
@@ -12,33 +8,34 @@ function solution(progresses, speeds) {
             progresses[i] += speeds[i];
             day++;
         }
-        q.push(day);
+        complete.push(day);
     }
-    console.log(q);
+    console.log(complete);
 
-    while (fnt < q.length - 1) {
-        if (q[fnt] > q[fnt + 1] || q[fnt] === q[fnt + 1]) {
-            num += 1;
-            fnt++;
-            if (num > 1 && fnt === q.length - 1) {
-                console.log('??');
-                bck === 0 ? (bck = 0) : (bck -= 1);
-                answer[bck] = num;
+    let fnt = 0;
+    let count = false;
+
+    while (fnt < complete.length - 1) {
+        if (complete[fnt] > complete[fnt + 1] || complete[fnt] === complete[fnt + 1]) {
+            if (!count) {
+                answer.push(1);
+                count = true;
+                console.log(complete[fnt], complete[fnt + 1], 'a');
+            } else {
+                last = answer.pop();
+                answer.push(last + 1);
+                console.log(complete[fnt], complete[fnt + 1], 'b');
             }
+            fnt += 1;
         } else {
-            if (num > 1) {
-                answer[bck] = num;
-                bck += 1;
-                num = 1;
-            }
-            answer[bck] = 1;
-            bck += 1;
+            answer.push(1);
+            count = false;
+            console.log(complete[fnt], complete[fnt + 1], 'c');
             fnt += 1;
         }
     }
+
     return answer;
 }
 
-// 선생님.... 제가... .. 잘못 풀고 있는 것 같아요..... 같은 것 처리를 대체 어ㄸ거더걷걷걷걷ㄲ 망했어 ....망..
-
-console.log(solution([5, 5, 5], [21, 25, 20]));
+console.log(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]));
